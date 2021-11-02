fb8e0204c76d03b68a1acb4c4b1b3471fa6adcf9
neighborhoodnft <neighborhoodnft21@gmail.com.com>
2021-11-02 15:57:05 -0700

    squashing commits

---
+++ precache-manifest.30ed3c0fcf8fa8b7dedb3e61e7286496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa41178e334b8e39d7c5",
+    "url": "/static/css/main.d9b17744.chunk.css"
+  },
+  {
+    "revision": "aa41178e334b8e39d7c5",
+    "url": "/static/js/main.aa41178e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0c4de936700496e38de3",
+    "url": "/static/css/2.3fbe635a.chunk.css"
+  },
+  {
+    "revision": "0c4de936700496e38de3",
+    "url": "/static/js/2.0c4de936.chunk.js"
+  },
+  {
+    "revision": "6c355c4510a16ff39e3260a1fa8e976a",
+    "url": "/index.html"
+  }
+];
